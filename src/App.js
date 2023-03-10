f27c612600245ea333e157e4d2bd2531347918df
Azim <azimreact@gmail.com>
2023-03-10 17:39:55 +0600

    create PostItem + PostList componets and states of the posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, {useState} from "react";
 import './styles/App.css'
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript 1', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'}
+    ])
+    const [posts1, setPosts1] = useState([
+        {id: 1, title: 'Python 1', body: 'Description'},
+        {id: 2, title: 'Python 2', body: 'Description'},
+        {id: 3, title: 'Python 3', body: 'Description'}
+    ])
 
   return (
     <div className="App">
-        <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>
-
+      <PostList posts={posts} title='Posts about JS'/>
+      <PostList posts={posts1} title='Posts about Python'/>
     </div>
   );
 }
