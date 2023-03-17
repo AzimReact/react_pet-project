e37539688d6387d19e4ee8861d672b2f533141d4
Azim <azimreact@gmail.com>
2023-03-17 15:00:52 +0600

    get posts from server with axios and render in page

--- src/App.js
+++ src/App.js
@@ -6,24 +6,31 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
-    const [modal, setModal] = useState('')
+    const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
 
+    async function fetchPosts () {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
+    }
+
+    // Получаем пост из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id ))
     }
 
   return (
     <div className="App">
+        <button onClick={fetchPosts}>Get Posts</button>
         <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Create user</MyButton>
         <MyModal visible={modal} setVisible={setModal}>
             <PostForm create={createPost}/>
