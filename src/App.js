33fbfe396987a35452d01953f3313bc5e163f455
Azim <azimreact@gmail.com>
2023-03-17 16:57:30 +0600

    create and use UI-comp Loader

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+    const [isPostLoading, setIsPostLoading] = useState(false)
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
@@ -24,8 +25,14 @@ function App() {
     }, [])
 
     async function fetchPosts () {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+        setIsPostLoading(true)
+        setTimeout(async () => {
+            const posts = await PostService.getAll()
+            setPosts(posts)
+            setIsPostLoading(false)
+        }, 500)
+
+
     }
 
     // Получаем пост из дочернего компонента
@@ -43,7 +50,10 @@ function App() {
 
         <hr style={{margin: '15px 0'}}/>
         <PostFilter filter={filter} setFilter={setFilter}/>
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts about JS'/>
+        {isPostLoading
+               ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50} }><Loader /></div>
+               : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts about JS'/>
+        }
     </div>
   );
 }
