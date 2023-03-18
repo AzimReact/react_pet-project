ee31b8577f3781a386fbe310ae3b84ba162b41f9
Azim <azimreact@gmail.com>
2023-03-18 13:39:40 +0600

    added pagination of our post, create reuseable component Pagination

--- src/App.js
+++ src/App.js
@@ -9,28 +9,43 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/paginaton/Pagination";
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
     })
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
 
     useEffect(() => {
-        fetchPosts()
+        fetchPosts(limit, page)
     }, [])
 
     // Получаем пост из дочернего компонента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id ))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts(limit, page)
     }
 
   return (
@@ -51,6 +66,7 @@ function App() {
                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50} }><Loader /></div>
                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts about JS'/>
         }
+        <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
     </div>
   );
 }
