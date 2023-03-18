129778b1e19d50886ab54607dc1638b1a9756511
Azim <azimreact@gmail.com>
2023-03-19 03:26:06 +0600

    useContext + authorization

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
-import About from "../pages/About";
-import Posts from "../pages/Posts";
-import Error from "../pages/Error";
-import PostIdPage from "../pages/PostIdPage";
 import {publicRoutes, privateRoutes} from "../router";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 const AppRouter = () => {
-    const isAuth = false;
+    const {isAuth, isLoading} = useContext(AuthContext)
+    if(isLoading) {
+        return <Loader />
+     }
     return (
             isAuth ?
                 <Routes>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import MyInput from "../components/UI/input/MyInput";
 import MyButton from "../components/UI/button/MyButton";
+import {AuthContext} from "../context";
 
 const Login = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext)
+    const login = event => {
+        event.preventDefault()
+        setIsAuth(true)
+        localStorage.setItem('auth', 'true')
+    }
     return (
         <div>
             <h1>Страница для логина</h1>
-            <form action="">
+            <form onSubmit={login}>
                 <MyInput type="text" placeholder='enter login'/>
                 <MyInput type="text" placeholder='enter password'/>
-                <MyButton >Enter</MyButton>
+                <MyButton>Log in</MyButton>
             </form>
         </div>
     );
