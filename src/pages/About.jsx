9bd6659ed7a0c5faad16af90fc77f51df3f9e94f
Azim <azimreact@gmail.com>
2023-03-18 14:29:24 +0600

    added routing, create pages and set up page navigation

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            This app is created for learning React by ULBI's courses!
+        </h1>
+    );
+};
+
+export default About;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <h1 style={{color: 'red'}}>
+            You have gone to a non-existent page.
+        </h1>
+    );
+};
+
+export default Error;
